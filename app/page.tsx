4c1b344e73d08d1772bdecc4fb2d85a14f00fb63
Christopher Mungiria <christopher.mungiria@tumeconsulting.com>
2024-10-23 11:37:08 +0300

    feat: sport categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+// Components
+import SportsCategories from "@/components/home/sports-categories";
+
 export default function Home() {
   return (
     <>
-      <h1>Sports categories</h1>
+      <SportsCategories />
       <h1>Blog List</h1>
     </>
   );
---
+++ components/home/sports-categories.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+// React
+import { useEffect, useState } from "react";
+
+// Actions
+import { fetchAllCategories } from "@/actions/category";
+
+// Components
+import { Button } from "../ui/button";
+
+const SportCategories = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [allCategories, setAllCategories] = useState<Category[] | null>();
+  const [error, setError] = useState<string | null>();
+
+  const fetchCategoriesFn = async () => {
+    try {
+      const categories = await fetchAllCategories();
+      console.log("Categories: ", categories.data);
+      if (categories.data) {
+        setAllCategories(categories.data);
+      } else {
+        setAllCategories(null);
+      }
+    } catch (error) {
+      console.error("Failed to fetch categories: ", error);
+      setError("Failed to fetch categories. Please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoriesFn();
+  }, []);
+
+  if (loading) {
+    const blocks = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+    return (
+      <div className="flex items-center gap-2">
+        {Array.from({ length: blocks }).map((_, index) => {
+          return (
+            <div
+              key={index}
+              className="h-10 w-32 bg-slate-200 animate-pulse rounded-md"
+            ></div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>;
+  }
+  return (
+    <div className="flex items-center gap-2 overflow-x-scroll">
+      {allCategories
+        ? allCategories.map((category, index) => (
+            <Button variant={"outline"} key={index}>
+              <p className="text-nowrap text-sm">
+                {category.icon} {category.title}
+              </p>
+            </Button>
+          ))
+        : null}
+    </div>
+  );
+};
+
+export default SportCategories;
