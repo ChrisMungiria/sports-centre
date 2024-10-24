fb89ff5759ed74af2762cf32aedccd0f793ead14
Christopher Mungiria <christopher.mungiria@tumeconsulting.com>
2024-10-24 10:04:52 +0300

    feat: sports category component

--- components/home/sports-categories.tsx
+++ components/home/sports-categories.tsx
@@ -1,87 +1,67 @@
 "use client";
 
-// React
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
-
 // Actions
 import { fetchAllCategories } from "@/actions/category";
+
+// React
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 // Components
 import { Button } from "../ui/button";
 
 // Props
-type SportCategoriesProps = {
-  selectedCategory: Category | undefined | null;
+type SportsCategoriesProps = {
+  selectedCategory: Category | null | undefined;
   setSelectedCategory: Dispatch<SetStateAction<Category | null | undefined>>;
 };
 
-const SportCategories = ({
+const SportsCategories = ({
   selectedCategory,
   setSelectedCategory,
-}: SportCategoriesProps) => {
+}: SportsCategoriesProps) => {
+  const [categories, setCategories] = useState<Category[] | null>();
   const [loading, setLoading] = useState<boolean>(true);
-  const [allCategories, setAllCategories] = useState<Category[] | null>();
-  const [error, setError] = useState<string | null>();
 
-  const fetchCategoriesFn = async () => {
+  const fetchAllCategoriesFn = async () => {
     try {
-      const categories = await fetchAllCategories();
-      console.log("Categories: ", categories.data);
-      if (categories.data) {
-        setAllCategories(categories.data);
-      } else {
-        setAllCategories(null);
+      const data = await fetchAllCategories();
+      if (data.data) {
+        setCategories(data.data);
       }
     } catch (error) {
-      console.error("Failed to fetch categories: ", error);
-      setError("Failed to fetch categories. Please try again later.");
+      console.log("Error fetching categories in client component: ", error);
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    fetchCategoriesFn();
+    fetchAllCategoriesFn();
   }, []);
 
-  if (loading) {
-    const blocks = Math.floor(Math.random() * (10 - 5 + 1)) + 5;
-    return (
-      <div className="flex items-center gap-2">
-        {Array.from({ length: blocks }).map((_, index) => {
-          return (
-            <div
-              key={index}
-              className="h-10 w-32 bg-slate-200 animate-pulse rounded-md"
-            ></div>
-          );
-        })}
-      </div>
-    );
-  }
+  if (loading) return <p>Loading...</p>;
 
-  if (error) {
-    return <div className="text-red-500">{error}</div>;
-  }
   return (
-    <div className="flex items-center gap-2 overflow-x-scroll">
-      {allCategories
-        ? allCategories.map((category, index) => (
-            <Button
-              key={index}
-              variant={selectedCategory === category ? "default" : "outline"}
-              onClick={() => {
-                setSelectedCategory(category);
-              }}
-            >
-              <p className="text-nowrap text-sm">
-                {category.icon} {category.title}
-              </p>
-            </Button>
-          ))
-        : null}
+    <div className="flex items-center gap-2">
+      {categories ? (
+        categories.map((category, index) => (
+          <Button
+            key={index}
+            variant={selectedCategory === category ? "default" : "outline"}
+            onClick={() => {
+              setSelectedCategory(category);
+            }}
+          >
+            {category.icon} {category.title}
+          </Button>
+        ))
+      ) : (
+        <p className="text-xs text-slate-400 text-center">
+          No categories in the database
+        </p>
+      )}
     </div>
   );
 };
 
-export default SportCategories;
+export default SportsCategories;
